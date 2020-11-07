fb83b87eb440413cf9da8ace6ec7573562d1ae54
Cdsr99 <cdsr073@gmail.com>
2020-11-06 23:41:20 -0300

    Update index.php

--- Desenvolvimento de aplicações/ToDo/index.php
+++ Desenvolvimento de aplicações/ToDo/index.php
@@ -10,14 +10,14 @@ require "src/classes/ArquivoTarefa.php";
 $template = file_get_contents('resource/lista_tarefas.html');
 
 $arquivoTarefa = new ArquivoTarefa('tarefas.txt');// envia para ArquivoTarefa.php
-$listaTarefasJSON = $arquivoTarefa->le();
+$listaTarefasJSON = $arquivoTarefa->le();// ler o arquivo e salvar em uma variavél 
+//$listaTarefasJSON recebe
+$modeloTarefa = file_get_contents('resource/tarefa.html');//?
 
-$modeloTarefa = file_get_contents('resource/tarefa.html');
-
-$linhasTabela = '';
+$linhasTabela = ''; //Limpar a variavel
 
 
-foreach ($listaTarefasJSON as $tarefa) {
+foreach ($listaTarefasJSON as $tarefa) { // as ?
     $tr = '';
     $tr = str_replace('#STATUS', $tarefa->legenda(), $modeloTarefa);
     $tr = str_replace('#ID',     $tarefa->getId(), $tr);
